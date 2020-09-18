ae78ff789abad3cc52f683a5c8168d9fd8fa6c05
Rusty Robot <rustyrobot@robot.com>
2020-09-18 09:14:43 +0100

    added objc project and unit test.

---
+++ objc_openssl_playground_app/objc_openssl_playground_appTests/objc_openssl_playground_appTests.m
@@ -0,0 +1,28 @@
+#import <XCTest/XCTest.h>
+
+@interface objc_openssl_playground_appTests : XCTestCase
+
+@end
+
+@implementation objc_openssl_playground_appTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    NSLog(@"[*] unit tests added");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
