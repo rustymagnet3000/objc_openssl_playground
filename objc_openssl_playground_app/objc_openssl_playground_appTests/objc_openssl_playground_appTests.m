0261f16d2702239f7adb45b39fe2a8b8ff9e9c8d
Rusty Robot <rustyrobot@robot.com>
2020-11-06 12:05:10 +0000

    Moved all the certs to a Folder Reference ( the blue icon ) so it was easy to drag into the Build Phases

--- objc_openssl_playground_app/objc_openssl_playground_appTests/objc_openssl_playground_appTests.m
+++ objc_openssl_playground_app/objc_openssl_playground_appTests/objc_openssl_playground_appTests.m
@@ -1,7 +1,7 @@
 @import XCTest;
 #import "YDOpenSSLManager.h"
 #import "YDOpenSSLHelper.h"
-
+#define MAX_TESTS 50
 
 @interface objc_openssl_playground_appTests : XCTestCase
 @end
@@ -12,7 +12,6 @@
 
     - (void)setUp {
         self.continueAfterFailure = NO;
-        manager = [[YDOpenSSLManager alloc] init];
     }
 
     - (void)banner{
@@ -28,11 +27,27 @@
 
     - (void)testInitVerifyAndPrintResponse {
         [self banner];
+        manager = [[YDOpenSSLManager alloc] init];
+         
         if (manager != nil){
-            [manager printServerResponse];
+            // [manager printServerResponse];
             [manager cleanUp];
         }
         [self banner];
+    }
+
+    - (void)testSpeedOfInit {
+        int successCount = 0;
+        for (int i = 0; i < MAX_TESTS; i++) {
+            manager = [[YDOpenSSLManager alloc] init];
+            if (manager != nil){
+                successCount++;
+                [manager cleanUp];
+            }
+        }
+        assert(successCount == MAX_TESTS);
+        NSLog(@"[*]Complete %d", successCount);
+        
     }
 
     - (void)testReadCertificate {
@@ -41,6 +56,8 @@
         [self banner];
     }
 
-    - (void)tearDown {}
+    - (void)tearDown {
+            //
+    }
 
 @end
